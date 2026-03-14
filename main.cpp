12dbcb398aa6657672e3198e1274495ac4e5d1cb
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-03-14 18:52:08 +0300

    Update main.cpp
    собирается.

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@
 #include <boost/asio.hpp>
 #include <memory>
 #include <atomic>
+#include "async.h"
 
 
 using boost::asio::ip::tcp;
 
-std::string message = "TCP/IP Hello world";
+std::string message = "TCP/IP Hello world\n";
 
 std::string message_hash = "1234";
 std::atomic_uint connection_id = 0;
+// Глобальная переменная для размера блока
+std::size_t g_bulk_size = 0;
 
 // IO контекст и acceptor (прослушивает порт 18000)
 boost::asio::io_context io_context;
@@ -23,21 +26,25 @@ tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 18000));
 
 // Класс, представляющий одно подключение
 struct Connection : public std::enable_shared_from_this<Connection>
-{
+{    
+
+   tcp::socket socket;
+  // std::size_t bulk_size_;
+   async::BulkContext* ctx_;
    
-    static std::shared_ptr<Connection> Create(boost::asio::io_context& io_context)
-    {
-        return std::make_shared<Connection>(io_context);
-    }
-
-
-
-    tcp::socket socket;
+   static std::shared_ptr<Connection> Create(boost::asio::io_context& io_context)
+   {
+      return std::make_shared<Connection>(io_context);
+   }
 
 
     Connection(boost::asio::io_context& io_context) : socket(io_context)
     {
-        
+        ctx_ = async::connect(g_bulk_size);
+    }
+    
+      ~Connection() {
+         async::disconnect(ctx_);  // при разрушении сессии удаляем контекст
     }
 };
 
@@ -52,7 +59,7 @@ void BeginAcceptConnection()
     std::shared_ptr<Connection> c = Connection::Create(io_context);
     //std::shared_ptr<tcp::socket> s = new tcp::socket(io_context);
     //acceptor.async_accept(c->socket, std::bind(HandleAccept,c,boost::asio::placeholders::error));
-        // Асинхронно ждём подключения.
+    // Асинхронно ждём подключения.
     // Вместо std::bind используем лямбда-захват shared_ptr c.
     acceptor.async_accept(c->socket,
         [c](const boost::system::error_code& err) {
@@ -111,8 +118,15 @@ void HandleWrite(const boost::system::error_code& err,size_t bytes_send)
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
+
+    if (argc != 3) {
+            std::cerr << "Usage: bulk_server <port> <bulk_size>\n";
+            return 1;
+    
+    }
+    
     std::cout << "SERVER PROGRAM" << std::endl;
     try
     {
@@ -129,5 +143,4 @@ int main()
 
     return 0;
 }
-
 
