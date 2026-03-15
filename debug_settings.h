5d67d30d60a5acc7fc4fa303deac3bb1d1569bc5
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-03-15 22:52:29 +0300

    Update debug_settings.h
    Отключен отладочный вывод.

--- debug_settings.h
+++ debug_settings.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // для быстрого включения/выключения отладочной информации.
-#define DEBUG 1  // 1 - включить, 0 - выключить
+#define DEBUG 0  // 1 - включить, 0 - выключить
 
 #if DEBUG
     #define DLOG(...) std::cerr << "DEBUG: " << __VA_ARGS__
