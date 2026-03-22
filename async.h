f31a5aaae13c4ebec59d4570975ddc3d59484e52
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-03-22 17:10:47 +0300

    Update async.h
    Добавлен  глобальный контекст для накопления команд между соединениями.

--- async.h
+++ async.h
@@ -4,20 +4,50 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <mutex>
+#include <memory>       //  для std::shared_ptr
 #include "debug_settings.h"
 
 namespace async {
+
 //добавляем сформированные блоки в очереди
 void add_block_to_queues(std::vector <std::string> block);
 void print_block_to_console(const std::vector<std::string>& cmds);
 void print_block_to_file(const std::vector<std::string>& cmds);
 
+// Глобальный контекст для накопления команд между соединениями
+struct GlobalContext {
+    std::vector<std::string> wait_commands;
+    std::mutex mutex;
+    std::size_t bulk_size;
+    GlobalContext(std::size_t size): bulk_size (size) {};
+    
+    void add_command(const std::string& cmd)
+    {
+      std::lock_guard <std::mutex> lock(mutex);
+      wait_commands.push_back(cmd);
+      if(wait_commands.size() >= bulk_size)
+      {
+          add_block_to_queues(wait_commands);
+          wait_commands.clear();
+      }    
+    }
+    void flush() {
+        std::lock_guard<std::mutex> lock(mutex);
+        if (!wait_commands.empty()) {
+            add_block_to_queues(wait_commands);
+            wait_commands.clear();
+        }
+    }
+};
+
 struct BulkContext {
     std::size_t bulk_size;
     std::vector<std::string> commands;
     int depth = 0;
+    std::shared_ptr<GlobalContext> global_ctx; // ссылка на глобальный контекст
 
-    BulkContext(std::size_t size) : bulk_size(size) {}
+    BulkContext(std::size_t size, std::shared_ptr<GlobalContext> global) : bulk_size(size), global_ctx(global) {}
     
     ~BulkContext() {
     
@@ -29,6 +59,11 @@ struct BulkContext {
           DLOG("destructor adding block of size " << commands.size() << std::endl);
           add_block_to_queues(commands);
       }
+      
+     if (depth == 0 && global_ctx) {
+        global_ctx->flush();
+     }
+      
       DLOG("BulkContext destructor END" << std::endl;)
     }
 
